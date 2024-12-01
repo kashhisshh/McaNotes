47ca6ee6064ca5220bd5928beda637d719d53db0
Kashish Narang <68736387+kashhisshh@users.noreply.github.com>
2024-12-01 19:16:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./navbar.css";
+import "./Navbar.css";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
